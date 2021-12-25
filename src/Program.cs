3f86fcf47ce7e3d7ab3307debda3d3928d3aec30
shassaan <syedhassaan.103@gmail.com>
2021-12-25 16:45:21 +0500

    [main] wrap program in one function

--- src/Program.cs
+++ src/Program.cs
@@ -2,43 +2,69 @@
 using safe_code_demo.Exceptions;
 using safe_code_demo.Extensions;
 
-try
+
+
+await StartPipeLineAsync();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async Task StartPipeLineAsync()
 {
-    await foreach (var line in Constants.INPUT_FILE_NAME.ExtractAsync())
+    try
     {
-        try
+        await foreach (var line in Constants.INPUT_FILE_NAME.ExtractAsync())
         {
-            await line
-            .Transform()
-            .LoadAsync(Constants.OUTPUT_FILE_NAME);
+            try
+            {
+                await line
+                .Transform()
+                .LoadAsync(Constants.OUTPUT_FILE_NAME);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"file not found {ex.FileName}");
+            }
+            catch (InvalidInputException ex)
+            {
+                Console.WriteLine(ex.Message);
+                continue;
+            }
+            catch (InvalidOutputException ex)
+            {
+                Console.WriteLine(ex.Message);
+                continue;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
-        catch (FileNotFoundException ex)
-        {
-            Console.WriteLine($"file not found {ex.FileName}");
-        }
-        catch (InvalidInputException ex)
-        {
-            Console.WriteLine(ex.Message);
-            continue;
-        }
-        catch (InvalidOutputException ex)
-        {
-            Console.WriteLine(ex.Message);
-            continue;
-        }
-        catch (Exception)
-        {
-            throw;
-        }
+
+
+    }
+    catch (FileNotFoundException ex)
+    {
+        Console.WriteLine($"file not found {ex.FileName}");
+    }
+    catch (Exception)
+    {
+        throw;
     }
 
-
-}
-catch (FileNotFoundException ex)
-{
-    Console.WriteLine($"file not found {ex.FileName}");
-}
-catch (Exception)
-{
-    throw;
 }
