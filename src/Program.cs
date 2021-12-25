1fa59a5aa90c249bbbabe40e7adb787cd9c6c2d5
shassaan <syedhassaan.103@gmail.com>
2021-12-25 13:54:26 +0500

    [master] add impl

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text;
+
+var inputLines = File.ReadAllLines("input.txt");
+var jsonBuilder = new StringBuilder();
+jsonBuilder.Append("[");
+foreach (var line in inputLines)
+{
+    var splits = line.Split(";");
+    jsonBuilder.Append("{");
+    jsonBuilder.Append($"\"firstName\":\"{splits[0]}\",");
+    jsonBuilder.Append($"\"lastName\":\"{splits[1]}\",");
+    jsonBuilder.Append($"\"phoneNumber\":\"{splits[2]}\"");
+    jsonBuilder.Append("},");
+}
+jsonBuilder.Append("]");
+File.WriteAllText("output.json",jsonBuilder.ToString());
+
+
+
