5f721ab49021ea8d2ccc68b29e38cc6834b48636
shassaan <syedhassaan.103@gmail.com>
2021-12-25 16:33:27 +0500

    [unsafe-code] print output on console

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,9 @@ var output = new List<string>();
 foreach (var line in inputLines)
 {
     var splits = line.Split(";");
-    output.Add($"{splits[0]} {splits[1]},{splits[2]}");
+    var outputString = $"{splits[0]} {splits[1]},{splits[2]}";
+    output.Add(outputString);
+    Console.WriteLine(outputString);
     
 }
 File.AppendAllLines("output.csv",output);
