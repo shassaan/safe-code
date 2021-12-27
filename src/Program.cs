92b1cea860a23b2350ee949f8aea4fd79ed44b16
shassaan <syedhassaan.103@gmail.com>
2021-12-27 23:12:30 +0500

    [unsafe-code] add clifx

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using CliFx;
+
+namespace safe_code_demo
+{
+    public static class Program
+    {
+        public static async Task<int> Main() =>
+            await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .Build()
+                .RunAsync();
+    }
+}
